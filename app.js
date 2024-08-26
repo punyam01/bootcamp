6f3cf19a4ef1f91c00fede9dc9738ead383fbbf9
punyam bhardwaj <punyamkumarsingh@gmail.com>
2024-08-26 13:08:23 +0530

    more functionality added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require ("express")
+const errorHandler = require("./middlewares/error.middleware.js")
 const app = express()
 
 
@@ -10,7 +11,8 @@ app.use(express.json());
 const bootcamp = require("./routes/bootcamp.routes.js")
 app.use('/api/v1/bootcamps', bootcamp)
 
-
+//error middleware
+app.use(errorHandler)
 
 
 module.exports = { app }
--- controllers/bootcamp.controller.js
+++ controllers/bootcamp.controller.js
@@ -1,10 +1,39 @@
 const Bootcamp = require("../models/bootcamp.model.js");
+const ErrorResponse = require ("../utils/errorREsponse.utils.js")
+exports.getAllBootcamp = async (req, res, next ) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({
+      success:true,
+      count:bootcamps.length,
+      data: bootcamps
+    })
+  } catch (error) {
+    next(new ErrorResponse(`unable to get the bootamps we got errors `, 400))
+    
+  }
+};
 
-exports.getAllBootcamp = (req, res) => {};
+exports.getBootcamp = async(req, res, next) => {
+  try {
+    const bootcamp =  await Bootcamp.findById(req.params.id);
+    if(!bootcamp){
+      return res.status(400).json({
+        success:false,
+        
+      })
+    }
+    res.status(200).json({
+      success:true,
+      data: bootcamp
+    })
+    
+  } catch (error) {
+    next(new ErrorResponse(`unable to get the bootamp with provided id we got errors `, 401))
+  }
+};
 
-exports.getBootcamp = (req, res) => {};
-
-exports.createBootcamp = async (req, res) => {
+exports.createBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.create(req.body); // Use 'bootcamp' instead of 'movie'
         res.status(201).json({
@@ -14,13 +43,49 @@ exports.createBootcamp = async (req, res) => {
           },
         });
       } catch (error) {
-        res.status(400).json({
-          status: "FAIL",
-          message: error.message,
-        });
+        next(new ErrorResponse(`we got errors `, 401))
       }
     };
 
-exports.updateBootcamp = (req, res) => {};
+exports.updateBootcamp = async(req, res,next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body,{
+      new:true,
+      runValidators:true
+    })
+    if(!bootcamp){
+      return res.status(400).json({
+        success:false,
+        
+      })
+    }
+    res.status(200).json({
+      success:true,
+      data: bootcamp
+    })
 
-exports.deleteBootcamp = (req, res) => {};
+
+  } catch (error) {
+    next(new ErrorResponse(`we got errors `, 400))
+  }
+};
+
+exports.deleteBootcamp = async(req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id)
+    if(!bootcamp){
+      return res.status(400).json({
+        success:false,
+        
+      })
+    }
+    res.status(200).json({
+      success:true,
+      data:{}
+    })
+
+
+  } catch (error) {
+    next(new ErrorResponse(`we got errors `, 400))
+  }
+};
