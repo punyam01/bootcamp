f2bc6835bc42c94039e8d6e284517b1c7d414a6a
punyam bhardwaj <punyamkumarsingh@gmail.com>
2024-08-27 12:27:22 +0530

    added async handler  middleware function

--- controllers/bootcamp.controller.js
+++ controllers/bootcamp.controller.js
@@ -1,76 +1,64 @@
+const asyncHandler = require("../middlewares/asyncHandler.middleware.js");
 const Bootcamp = require("../models/bootcamp.model.js");
 const ErrorResponse = require("../utils/errorREsponse.utils.js");
-exports.getAllBootcamp = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
-    res.status(200).json({
-      success: true,
-      count: bootcamps.length,
-      data: bootcamps,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
-exports.getBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findById(req.params.id);
-    if (!bootcamp) {
-      return next(new ErrorResponse(`unable to get the bootamps by provided id `, 404))
-    }
-    res.status(200).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.getAllBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
 
-exports.createBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body); // Use 'bootcamp' instead of 'movie'
-    res.status(201).json({
-      status: "SUCCESS",
-      data: {
-        bootcamp, // Return 'bootcamp' data in response
-      },
-    });
-  } catch (error) {
-    next(error);
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`unable to get the bootamps by provided id `, 404)
+    );
   }
-};
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
-exports.updateBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!bootcamp) {
-      return next(new ErrorResponse(`unable to get the bootamps by provided id `, 404))
-    }
-    res.status(200).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body); // Use 'bootcamp' instead of 'movie'
+  res.status(201).json({
+    status: "SUCCESS",
+    data: {
+      bootcamp, // Return 'bootcamp' data in response
+    },
+  });
+});
 
-exports.deleteBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id);
-    if (!bootcamp) {
-      return next(new ErrorResponse(`unable to get the bootamps by provided id `, 404))
-    }
-    res.status(200).json({
-      success: true,
-      data: {},
-    });
-  } catch (error) {
-    next(error);
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`unable to get the bootamps by provided id `, 404)
+    );
   }
-};
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
+
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`unable to get the bootamps by provided id `, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,21 +1,32 @@
 const ErrorResponse = require("../utils/errorREsponse.utils");
 
-const errorHandler =(err,req,res,next)=>{
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
 
-    let error = {...err}
-
-if (err.name=="CastError"){
-    const message = `Bootcamp not found with id of ${err.value}`;
+  // wrong id provided
+  if (err.name == "CastError") {
+    console.log("hii");
+    
+    const message = `Bootcamp not found with id of ${error.value}`;
     error = new ErrorResponse(message, 404);
-}
+  }
 
+  
+  // mongoose Duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
 
+  // mongoose validation error
+  if (err.name == "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 404);
+  }
 
-
-
-res.status(error.statusCode || 500).json({
-    success:false,
-    error:error.message || "Server error "
-})
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server error ",
+  });
 };
 module.exports = errorHandler;
